f1cf1e8d6fad20ff035342cc30520e0d82363108
showyha08 <over.and.uver@gmail.com>
2023-06-17 12:47:15 +0900

    first_commit

---
+++ app/src/main/java/com/websarva/wings/android/lifecyclesample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.websarva.wings.android.lifecyclesample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
