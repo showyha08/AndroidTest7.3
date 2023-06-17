e34d59dee752d02f268ba2a323dbc3650f12ad41
showyha08 <over.and.uver@gmail.com>
2023-06-17 13:43:08 +0900

    mod_MainActivity

--- app/src/main/java/com/websarva/wings/android/lifecyclesample/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/lifecyclesample/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.websarva.wings.android.lifecyclesample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i("LifeCycleSample","Main onCreate() called.")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    public override fun onStart(){
+        Log.i("LifeCycleSample","Main onStart() called.")
+        super.onStart()
+    }
+    public override fun onRestart(){
+        Log.i("LifeCycleSample","Main onRestart() called.")
+        super.onRestart()
+    }
+
+    public override fun onResume(){
+        Log.i("LifeCycleSample","Main onResume() called.")
+        super.onResume()
+    }
+
+    public override fun onPause(){
+        Log.i("LifeCycleSample","Main onPause() called.")
+        super.onPause()
+    }
+
+    public override fun onStop(){
+        Log.i("LifeCycleSample","Main onStop() called.")
+        super.onStop()
+    }
+
+    public override fun onDestroy(){
+        Log.i("LifeCycleSample","Main onDestroy() called.")
+        super.onDestroy()
+    }
+
+    // 次の画面を表示ボタンがタップされたとき
+    fun onButtonClick(view: View){
+        //インテントオブジェクトを用意
+        val intent = Intent(this@MainActivity,SubActivity::class.java)
+        //アクティビティを起動
+        startActivity(intent)
     }
 }
